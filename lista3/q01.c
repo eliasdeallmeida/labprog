7a0a9beef578959b15ea6bf42ccc499edafdb413
Elias de Almeida <eliasombr@gmail.com>
2022-09-19 21:05:32 -0300

    Add question 1 list 3

---
+++ lista3/q01.c
@@ -0,0 +1,36 @@
+// Faça um programa em C que leia um vetor do tipo float com 15 elementos e apresente a soma do menor e maior elemento do vetor fornecido.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 15
+#define MAX 10
+
+int main(void){
+    float maior, menor, soma, valor[TAM];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < TAM; i++){
+        valor[i] = rand() / (float)RAND_MAX * MAX;
+
+        if (i == 0){
+            maior = menor = valor[i];
+        }
+        else{
+            if (valor[i] > maior)
+                maior = valor[i];
+            else if (valor[i] < menor)
+                menor = valor[i];
+        }
+
+        printf("%dº número = %.2f\n", i + 1, valor[i]);
+    }
+
+    soma = maior + menor;
+
+    printf("O maior valor é %.2f e o menor valor é %.2f e a soma entre eles vale %.2f\n", maior, menor, soma);
+
+    return 0;
+}
