3216898b5d6c34496aef1ceff680f8c90222c39e
Elias de Almeida <eliasombr@gmail.com>
2022-09-28 21:32:36 -0300

    Update challenge 1
    - Add occupy locker function
    - Add release locker function
    - Change variable type

--- desafios/desafio01.c
+++ desafios/desafio01.c
@@ -5,13 +5,13 @@
 
 #define TOTAL 8
 
-int main(void){
-    unsigned char armario = 0;
-    int opcao, posicao, maximo;
+unsigned char ocupaArmario(unsigned char armario);
+unsigned char desocupaArmario(unsigned char armario);
 
-    maximo = pow(2, TOTAL) - 1;
+int main(void){
+    unsigned char armario = 0, opcao, posicao;
+
     srand(time(NULL));
-    
     printf("===== SISTEMA DE ARMÁRIOS IFCE =====\n");
     
     do{
@@ -25,45 +25,57 @@ int main(void){
         puts("(1) Ocupar armário");
         puts("(2) Liberar armário");
         puts("(3) Sair");
-        
         printf(">>> Sua opção: ");
-        scanf("%d", &opcao);
+        scanf("%hhu", &opcao);
 
         switch(opcao){
-            case 1:
-                if(armario == maximo){
-                    printf("\nTodos os armários estão ocupados!\n");
-                    break;
-                }
-                else{
-                    do{
-                        posicao = rand() % TOTAL;
-                    }while((armario & (int)pow(2, posicao)) != 0);
-                }
-                armario |= (int)pow(2, posicao);
-                printf("\nSeu armário é o de número %d.\n", posicao);
-                break;
-            case 2:
-                printf(">>> De 0 a 7, qual armário deseja liberar? ");
-                scanf("%d", &posicao);
-                if(posicao < 0 || posicao > TOTAL - 1){
-                    printf("\nNúmero de armário inválido!\n");
-                }
-                else if((armario & (int)pow(2, posicao)) == 0){
-                    printf("\nO armário %d já está desocupado.\n", posicao);
-                }
-                else{
-                    armario &= ~(int)pow(2, posicao);
-                    printf("\nArmário %d foi liberado com sucesso!\n", posicao);
-                }
-                break;
-            case 3:
-                printf("\nPrograma encerrado.\n");
-                break;
-            default:
-                printf("\nOpção inválida!\n");
+            case 1: armario = ocupaArmario(armario); break;
+            case 2: armario = desocupaArmario(armario); break;
+            case 3: printf("\nPrograma encerrado.\n"); break;
+            default: printf("\nOpção inválida!\n");
         }
     }while(opcao != 3);
     
     return 0;
+}
+
+unsigned char ocupaArmario(unsigned char armario){
+    unsigned char numero, posicao, maximo;
+    maximo = pow(2, TOTAL) - 1;
+
+    if(armario == maximo){
+        printf("\nTodos os armários estão ocupados!\n");
+    }
+    else{
+        do{
+            numero = rand() % TOTAL;
+            posicao = pow(2, numero);
+        }while((armario & posicao) != 0);
+        armario |= posicao;
+        printf("\nSeu armário é o de número %hhu.\n", numero);
+    }
+
+    return armario;
+}
+
+unsigned char desocupaArmario(unsigned char armario){
+    unsigned char numero, posicao;
+
+    printf(">>> De 0 a 7, qual armário deseja liberar? ");
+    scanf("%hhu", &numero);
+
+    posicao = pow(2, numero);
+
+    if(numero < 0 || numero >= TOTAL){
+        printf("\nNúmero de armário inválido!\n");
+    }
+    else if((armario & posicao) == 0){
+        printf("\nO armário %d já está desocupado.\n", numero);
+    }
+    else{
+        armario &= ~posicao;
+        printf("\nArmário %hhu foi liberado com sucesso!\n", numero);
+    }
+
+    return armario;
 }
